d062ba8f8c8bed21a498e9878db5b73f0f8b1758
P.Prithvi Akhil <50729566+pruthviakhil@users.noreply.github.com>
2019-05-20 01:29:49 +0530

    Add files via upload

---
+++ 16.2LCD interfacing.c.c
@@ -0,0 +1,25 @@
+/*
+ * we.c
+ *
+ * Created: 5/19/2019 6:20:09 PM
+ * Author : pruthvi akhil
+ */ 
+
+#ifndef  F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRB= 0xFF;
+	while (1)
+	{
+		PORTB = 0X55;
+		_delay_ms(1000);
+		PORTB = 0XAA;
+		_delay_ms(1000);
+	}
+}
